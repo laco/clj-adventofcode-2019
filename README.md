d3ef96c1536b778a4c516dc58451158985379de2
László Andrási <mail@laszloandrasi.com>
2019-12-01 13:55:05 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# clj-adventofcode-2019
+
+Advent of Code Challenge in Clojure (2019) 
+
---
+++ src/clj_adventofcode_2019/core.clj
@@ -0,0 +1,7 @@
+(ns clj-adventofcode-2019.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
