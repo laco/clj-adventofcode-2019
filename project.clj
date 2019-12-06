fa90f96eabc9c97bc324b077c7128bd265e27091
László Andrási <mail@laszloandrasi.com>
2019-12-06 14:27:54 +0100

    day05

--- project.clj
+++ project.clj
@@ -3,7 +3,9 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.10.1"]]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [org.clojure/math.numeric-tower "0.0.4"]
+                 ]
   :main ^:skip-aot clj-adventofcode-2019.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
