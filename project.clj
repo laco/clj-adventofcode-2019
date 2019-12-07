bbd3bc275a8466cd26bee3d341047f9b274f0393
László Andrási <mail@laszloandrasi.com>
2019-12-07 17:55:36 +0100

    day07 part 1

--- project.clj
+++ project.clj
@@ -5,6 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure/math.numeric-tower "0.0.4"]
+                 [org.clojure/math.combinatorics "0.1.6"]
                  ]
   :main ^:skip-aot clj-adventofcode-2019.core
   :target-path "target/%s"
